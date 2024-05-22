0ad6dbeb582d47601d70a10e432c295c2f5b1a5f
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2024-05-22 16:16:10 +0900

    Add changing tab

--- tui/status.go
+++ tui/status.go
@@ -13,3 +13,77 @@ const (
 	// statusPrivateKeySaveErr is the status when the private key save error occurs.
 	statusPrivateKeySaveErr
 )
+
+// currentView is the current view of the honeycomb operation.
+type currentView int
+
+const (
+	// currentVierTimeline is the current view when the timeline view is displayed.
+	currentViewTimeline currentView = iota
+	// currentViewTrend is the current view when the trend view is displayed.
+	currentViewTrend
+	// currentViewFollow is the current view when the follow view is displayed.
+	currentViewFollow
+	// currentViewFollower is the current view when the follower view is displayed.
+	currentViewFollower
+	// currentViewProfile is the current view when the profile view is displayed.
+	currentViewProfile
+	// currentViewSetting is the current view when the setting view is displayed.
+	currentViewSetting
+)
+
+// next moves the current view to the next view.
+func (c *currentView) next() {
+	*c++
+	if *c > currentViewSetting {
+		*c = currentViewTimeline
+	}
+}
+
+// prev moves the current view to the previous view.
+func (c *currentView) prev() {
+	*c--
+	if *c < currentViewTimeline {
+		*c = currentViewSetting
+	}
+}
+
+// string returns the string representation of the current view.
+func (c currentView) string() string {
+	switch c {
+	case currentViewTimeline:
+		return "Timeline"
+	case currentViewTrend:
+		return "Trend"
+	case currentViewFollow:
+		return "Follow"
+	case currentViewFollower:
+		return "Follower"
+	case currentViewProfile:
+		return "Profile"
+	case currentViewSetting:
+		return "Setting"
+	default:
+		return ""
+	}
+}
+
+// stringWithBee returns the string representation of the current view with the bee.
+func (c *currentView) stringWithBee() string {
+	switch *c {
+	case currentViewTimeline:
+		return "🐝  Timeline  🐝"
+	case currentViewTrend:
+		return "🐝  Trend  🐝"
+	case currentViewFollow:
+		return "🐝  Follow  🐝"
+	case currentViewFollower:
+		return "🐝  Follower  🐝"
+	case currentViewProfile:
+		return "🐝  Profile  🐝"
+	case currentViewSetting:
+		return "🐝  Setting  🐝"
+	default:
+		return ""
+	}
+}
